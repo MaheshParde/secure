4eff4151b8f8c80d338f3f69d1a6435bf00cc4dd
LAPTOP-58L05II1\parde <pardemahesh9595@gmai.com>
2021-01-31 21:00:25 +0530

    error

--- src/main/java/AddTwoNumbers.java
+++ src/main/java/AddTwoNumbers.java
@@ -1,7 +1,11 @@
+import java.util.List;
+
 public class AddTwoNumbers {
     public static void main(String[] args){
         int num1=10,num2=20,sum;
         sum=num1+num2;
-        System.out.println("Sum of two numbers:"+sum);
+        List<Integer> numList=null;
+        numList.add(sum);
+        System.out.println("Sum of two numbers:"+numList);
     }
 }
